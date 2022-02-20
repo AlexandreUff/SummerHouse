5f60b509f01d381b6a42ecacadb92fa3282c0548
AlexandreUff <97979545+AlexandreUff@users.noreply.github.com>
2022-02-19 21:53:06 -0300

    5º Commit: Inserção da mensagem de erro
    - Inserção da mensagem de erro para caso hajam senhas diferentes nos campos "Senha" e "Repetir Senha";
    - Estilização da função de Erro;

--- account/register-code.js
+++ account/register-code.js
@@ -10,11 +10,59 @@ const slider = take('.slider');
 const prevSlide = take('#prev-slide');
 const nextSlide = take('#next-slide');
 const mostradorSlide = take('.buttons-slide small');
+
+const Formulario = take('form');
+
 const checkCPF = take('#no-cpf-usuario');
 const checkRG = take('#no-rg-usuario');
+
 const clearButton = take('#clear-button');
+const btnEnviar = take('#btn-submit');
 
 let contadorSlider = 0;
+
+/* Talvez esta função seja desabilitada */
+function mensagemDeNaoPreenchimento(inp,mensagem){
+    const msgErro = document.createElement('small');
+    msgErro.classList.add('input-error');
+
+    if(inp.nextSibling.nodeName !== msgErro.nodeName){
+        msgErro.innerHTML = mensagem;
+        inp.after(msgErro);
+    }
+}
+
+function verificaPreenchimentoFormulario(){
+
+    const inputNome = take('#nome-usuario');
+    const inputSobrenome = take('#sobrenome-usuario');
+    const inputNasc = take('#nasc-usuario');
+    const inputCPF = take('#cpf-usuario');
+    const inputRG = take('#rg-usuario');
+    const inputEmail = take('#email-usuario');
+    const inputPws = take('#psw-usuario');
+    const inputRpPws = take('#rp-psw-usuario');
+
+
+    if(inputNome.value && inputSobrenome.value && inputNasc.value
+        && (inputCPF.value || inputCPF.disabled) && (inputRG.value || inputRG.disabled) && inputEmail.value
+        && inputPws.value && inputRpPws.value /* && (inputPws.value === inputRpPws.value) */){
+        
+        console.log('Preenchido');
+
+        if(inputPws.value !== inputRpPws.value){
+            btnEnviar.disabled = true
+            mensagemDeNaoPreenchimento(inputRpPws, 'As senhas estão diferentes.');
+        } else {
+            btnEnviar.disabled = false
+        }
+    } else {
+        btnEnviar.disabled = true
+        /* mensagemDeNaoPreenchimento(inputNome); */
+    }
+}
+
+Formulario.addEventListener('change', verificaPreenchimentoFormulario);
 
 function checkDocs(){
 
@@ -35,6 +83,7 @@ clearButton.addEventListener('click',()=>{
     const inputRG = take('#rg-usuario');
     inputCPF.disabled = false
     inputRG.disabled = false
+    btnEnviar.disabled = true
 });
 
 function moveSlide(num,tipo){
